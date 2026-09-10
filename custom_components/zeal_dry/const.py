505831b862eff270fbef4189bd26a2baef93763d
Brent Brown <Buster1959@users.noreply.github.com>
2026-09-10 22:47:50 +0100

    block 1: add constants

---
+++ custom_components/zeal_dry/const.py
@@ -0,0 +1,9 @@
+"""Constants for ZEAL-Dry."""
+
+DOMAIN = "zeal_dry"
+
+CONF_ZONE_NAME = "zone_name"
+CONF_TEMPERATURE_ENTITY = "temperature_entity"
+CONF_HUMIDITY_ENTITY = "humidity_entity"
+
+DATA_CONTROLLERS = "controllers"
