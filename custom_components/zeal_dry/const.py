fa03581328f8e357b1a6418b66bbbcd553bea57f
Brent Brown <Buster1959@users.noreply.github.com>
2026-09-11 13:16:15 +0100

    feat: add control mode constants

--- custom_components/zeal_dry/const.py
+++ custom_components/zeal_dry/const.py
@@ -5,5 +5,10 @@ DOMAIN = "zeal_dry"
 CONF_ZONE_NAME = "zone_name"
 CONF_TEMPERATURE_ENTITY = "temperature_entity"
 CONF_HUMIDITY_ENTITY = "humidity_entity"
+CONF_CONTROL_MODE = "control_mode"
+
+CONTROL_MODE_MONITOR = "monitor_only"
+CONTROL_MODE_DUMMY = "dummy_acu"
+DEFAULT_CONTROL_MODE = CONTROL_MODE_MONITOR
 
 DATA_CONTROLLERS = "controllers"
